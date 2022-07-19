34d1600ee9be63fbc7026079befdeebd7a878cf7
trickaugusto <trickaugusto1206@gmail.com>
2022-07-19 15:30:52 -0300

    feat: ending class two

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,14 +1,36 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e gestão'
+    ];
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault(); // retira o comportamento padrão da página, ou seja, não vai recarregar no submit
+        console.log("Form foi submetido");
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto obrigatorio={false} label="Imagem" placeholder="Informe o endereço da imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     )
